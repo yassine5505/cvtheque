398a2097c0521de8a8ff600cad6a5a6d0b5a939b
Mohammed Yassine Chraibi <medyassinechraibi@gmail.com>
2019-01-09 23:37:45 +0100

    Modified top nav: removed search bar, and message dropdown

--- includes/topnav.php
+++ includes/topnav.php
@@ -1,22 +1,12 @@
-<nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+<nav class="navbar navbar-expand navbar-dark bg-dark static-top d-flex justify-content-between">
 
-       <a class="navbar-brand mr-1" href="index.html">Start Bootstrap</a>
+      <div class="navbar-brand-and-bars">
+        <a class="navbar-brand mr-1" href="index.html">CVtheque</a>
 
-       <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
-         <i class="fas fa-bars"></i>
-       </button>
-
-       <!-- Navbar Search -->
-       <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-         <div class="input-group">
-           <input type="text" class="form-control" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-           <div class="input-group-append">
-             <button class="btn btn-primary" type="button">
-               <i class="fas fa-search"></i>
-             </button>
-           </div>
-         </div>
-       </form>
+        <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+          <i class="fas fa-bars"></i>
+        </button>
+      </div>
 
        <!-- Navbar -->
        <ul class="navbar-nav ml-auto ml-md-0">
@@ -26,18 +16,6 @@
              <span class="badge badge-danger">9+</span>
            </a>
            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="alertsDropdown">
-             <a class="dropdown-item" href="#">Action</a>
-             <a class="dropdown-item" href="#">Another action</a>
-             <div class="dropdown-divider"></div>
-             <a class="dropdown-item" href="#">Something else here</a>
-           </div>
-         </li>
-         <li class="nav-item dropdown no-arrow mx-1">
-           <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-             <i class="fas fa-envelope fa-fw"></i>
-             <span class="badge badge-danger">7</span>
-           </a>
-           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="messagesDropdown">
              <a class="dropdown-item" href="#">Action</a>
              <a class="dropdown-item" href="#">Another action</a>
              <div class="dropdown-divider"></div>
