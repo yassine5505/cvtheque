5f390bcba342e2b53f44552efdab4647ceb43638
Mohammed Yassine Chraibi <medyassinechraibi@gmail.com>
2019-01-09 23:11:03 +0100

    Modified project architecture. Added js and css source files.

---
+++ includes/scripts.php
@@ -0,0 +1,18 @@
+<!-- Bootstrap core JavaScript-->
+<script src="../sources/vendor/jquery/jquery.min.js"></script>
+<script src="../sources/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="../sources/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Page level plugin JavaScript-->
+<script src="../sources/vendor/chart.js/Chart.min.js"></script>
+<script src="../sources/vendor/datatables/jquery.dataTables.js"></script>
+<script src="../sources/vendor/datatables/dataTables.bootstrap4.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="../sources/js/sb-admin.min.js"></script>
+
+<!-- Demo scripts for this page-->
+<script src="../sources/js/demo/datatables-demo.js"></script>
+<script src="../sources/js/demo/chart-area-demo.js"></script>
