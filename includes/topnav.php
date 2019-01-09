cec6b0359533929dee06d8214ba492e6a635e544
Mohammed Yassine Chraibi <medyassinechraibi@gmail.com>
2019-01-09 23:41:40 +0100

    Modified top navbar to dynamically show login or user action page.

--- includes/topnav.php
+++ includes/topnav.php
@@ -27,10 +27,22 @@
              <i class="fas fa-user-circle fa-fw"></i>
            </a>
            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-             <a class="dropdown-item" href="#">Settings</a>
-             <a class="dropdown-item" href="#">Activity Log</a>
-             <div class="dropdown-divider"></div>
-             <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
+             <?php
+              if (isset($_SESSION['connected'])){
+                echo '
+                  <a class="dropdown-item" href="#">Login</a>
+                  <a class="dropdown-item" href="#">Register</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
+                ';
+              }
+              else{
+                echo '
+                <a class="dropdown-item" href="#">Se connected</a>
+                <a class="dropdown-item" href="#">S\'inscrire</a>
+                ';
+              }
+              ?>
            </div>
          </li>
        </ul>
