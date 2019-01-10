f5c43a213663c029231f0c184aedfd2198f37475
Mohammed Yassine Chraibi <medyassinechraibi@gmail.com>
2019-01-10 22:01:16 +0100

    Updated db script

--- tkhawar/cvtheque.sql
+++ tkhawar/cvtheque.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 10, 2019 at 08:52 PM
+-- Generation Time: Jan 10, 2019 at 09:00 PM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -28,10 +28,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `competences` (
   `id` int(11) NOT NULL,
-  `competence` varchar(255) DEFAULT NULL,
-  `niveau` varchar(100) DEFAULT NULL,
+  `competence` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `niveau` varchar(100) CHARACTER SET latin1 DEFAULT NULL,
   `etudiant_id` int(11) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -53,12 +53,12 @@ CREATE TABLE `competences_requises` (
 
 CREATE TABLE `entreprises` (
   `id` int(11) NOT NULL,
-  `nom` char(30) DEFAULT NULL,
-  `adresse` char(60) DEFAULT NULL,
-  `phone` char(20) DEFAULT NULL,
-  `description` varchar(255) NOT NULL,
-  `logo` char(255) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `nom` char(30) CHARACTER SET latin1 DEFAULT NULL,
+  `adresse` char(60) CHARACTER SET latin1 DEFAULT NULL,
+  `phone` char(20) CHARACTER SET latin1 DEFAULT NULL,
+  `description` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `logo` char(255) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `entreprises`
@@ -77,14 +77,14 @@ INSERT INTO `entreprises` (`id`, `nom`, `adresse`, `phone`, `description`, `logo
 
 CREATE TABLE `etudiants` (
   `id` int(11) NOT NULL,
-  `numero_apogee` varchar(255) NOT NULL,
-  `nom` varchar(60) NOT NULL,
-  `prenom` varchar(60) NOT NULL,
-  `phone` varchar(30) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `image` varchar(255) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `numero_apogee` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `nom` varchar(60) CHARACTER SET latin1 NOT NULL,
+  `prenom` varchar(60) CHARACTER SET latin1 NOT NULL,
+  `phone` varchar(30) CHARACTER SET latin1 NOT NULL,
+  `email` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `description` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `image` varchar(255) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `etudiants`
@@ -92,7 +92,7 @@ CREATE TABLE `etudiants` (
 
 INSERT INTO `etudiants` (`id`, `numero_apogee`, `nom`, `prenom`, `phone`, `email`, `description`, `image`) VALUES
 (1, 'gh444', 'Mehdi', 'Chaou', '0654889977', '', 'Genie informatique 1 - Interesse par le developpement web', 'mehdi-chaert.jpg'),
-(2, 'Saad', 'Belgnaoui', 'Saad', '0699441122', '', 'ElÃ¨ve ingÃ©nieur 3Ã¨me annÃ©e Informatique.\r\nEsprit d\'entreprenariat.', 'saad-belgnaoui.php'),
+(2, 'Saad', 'Belgnaoui', 'Saad', '0699441122', '', 'ElÃ¨ve ingÃ©nieur 3Ã¨me annÃ©e Informatique.\nEsprit d\'entreprenariat.', 'saad-belgnaoui.jpg'),
 (3, '447', 'Reda', 'Ennakouri', '0600098419', '', 'Etudiant GI1', 'zuck.jpg'),
 (4, '447788', 'Yassine', 'Chra', '022114455', '', 'Genie info 2', 'zuck.jpg'),
 (5, '741', 'Abdellah', 'Benkirane', '0669874521', '', 'Description de Abdellah benkirane\'d', 'random.jpg');
@@ -106,9 +106,9 @@ INSERT INTO `etudiants` (`id`, `numero_apogee`, `nom`, `prenom`, `phone`, `email
 CREATE TABLE `offres` (
   `id` int(11) NOT NULL,
   `entreprise_id` int(11) DEFAULT NULL,
-  `intitule` char(40) DEFAULT NULL,
-  `duree` char(20) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `intitule` char(40) CHARACTER SET latin1 DEFAULT NULL,
+  `duree` char(20) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `offres`
