1ff16982e11de113f655e51dd5c4b14d158b6872
Mohammed Yassine Chraibi <medyassinechraibi@gmail.com>
2019-01-10 01:07:58 +0100

    removed securite/db.php and added code to config/dbConnection.php

--- config/dbConnection.php
+++ config/dbConnection.php
@@ -1,1 +1,16 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=cvtheque", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Base de données connectée !";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
