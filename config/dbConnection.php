2aa13e057aecfe471e03e501c45c8254333b9cc9
GitHubMehdii <mehdi.mc60@gmail>
2019-01-12 06:55:29 +0100

    end project

--- config/dbConnection.php
+++ config/dbConnection.php
@@ -1,10 +1,18 @@
 <?php
+/*
 $servername = "sql305.epizy.com";
 $username = "epiz_23280423";
 $password = "9pk8YdSTsPDw";
+$dbname="epiz_23280423_cvtheque";
+*/
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname="cvtheque";
+
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=epiz_23280423_cvtheque", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //echo "Base de données connectée !";
